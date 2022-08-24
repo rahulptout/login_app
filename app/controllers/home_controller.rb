a67333462f1096f3b8ad12b623b17da6134e3ee2
rahulptout <rahulp.tout@gmail.com>
2022-08-24 20:14:52 +0530

    24/08/2022

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,49 @@
 class HomeController < ApplicationController
   def index
+    @users = User.all 
+      
+   
   end
+
+  def show
+    @user = User.find(params[:id])
+  end  
+
+  def new 
+    @user = User.new
+
+  end
+  
+
+  def create
+     
+  end
+
+
+  def edit
+      redirect_to new_user_session_url 
+  end
+    
+
+  def update
+    @user = User.find(params[:id])
+
+    redirect_to edit_user_registration
+  
+
+  end
+
+
+  def destroy
+
+   @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to root_path, status: :see_other
+
+  end  
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
-  get 'home/index'
-  devise_for :views
+  devise_for :users, path: 'user', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  resources :home
+  # devise_for :views
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
